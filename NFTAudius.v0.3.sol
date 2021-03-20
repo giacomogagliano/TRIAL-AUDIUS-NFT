316cd75ba3c7cfb3bca2035abb9b8c06f484c5f3
Giacomo <WAW@WAW.homenet.telecomitalia.it>
2021-03-20 01:26:55 +0100

    V 0.3
    With this version I am trying to fit the JSON file to the schema given in the eip

---
+++ NFTAudius.v0.3.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity <0.8.0;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v3.4.0/contracts/token/ERC721/ERC721.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v3.4.0/contracts/utils/Counters.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v3.4.0/contracts/access/Ownable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v3.4.0/contracts/token/ERC721/IERC721Metadata.sol";
+
+contract NFTAudius is ERC721, Ownable {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+    
+    constructor()
+    ERC721("NFT from Audius files v0.3","NFTAudiusv0.2") {
+        _setBaseURI("https://my-json-server.typicode.com/giacomogagliano/TRIAL-AUDIUS-NFT/v0.3");
+    }
+    
+    function mintNft(address receiver) external onlyOwner returns (uint256) {
+        _tokenIds.increment();
+
+        uint256 newNftTokenId = _tokenIds.current();
+        _mint(receiver, newNftTokenId);
+
+        return newNftTokenId;
+    }
+}
